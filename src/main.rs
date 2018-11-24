69d4d70266e91fa5acfa05e08696f69a9ad8b397
Caleb Anderson <robotrising@gmail.com>
2018-11-24 13:32:27 -0700

    mini_gl_fb test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+extern crate mini_gl_fb;
+use mini_gl_fb::glutin::VirtualKeyCode;
+
 fn main() {
-    println!("Hello, world!");
+    let mut fb = mini_gl_fb::gotta_go_fast("Hello world!", 800.0, 600.0);
+
+    let mut noise;
+    let mut carry;
+    let mut seed = 0xbeefu32;
+    let mut buffer = vec![[255u8, 0, 0, 255]; 800 * 600];
+
+    
+
+    loop {
+        for i in 0..buffer.len() {
+            noise = seed;
+            noise >>= 3;
+            noise ^= seed;
+            carry = noise & 1;
+            noise >>= 1;
+            seed >>= 1;
+            seed |= carry << 30;
+            noise &= 0xFF;
+            buffer[i] = [noise as u8, noise as u8, noise as u8, 255];
+        }
+        fb.update_buffer(&buffer);
+        fb.redraw();
+        fb.glutin_handle_basic_input(|_fb, input| {
+            if input.key_is_down(VirtualKeyCode::Escape) {
+                std::process::exit(0);
+            }
+            false
+        });
+    }
 }
